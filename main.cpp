b47553e2d2b142c2fc32e131d4788a7a4a40f046
Nikolay Matkheev <nicola.matheev@gmail.com>
2015-12-08 20:16:46 +0300

    Update according C++11

--- main.cpp
+++ main.cpp
@@ -1,57 +1,76 @@
-// Jacobi.cpp : Defines the entry point for the console application.
-//
 
 #include <iostream>
-#include <cmath>
 #include <fstream>
+
+#include <cmath>
 #include <omp.h>
-#include <string.h>
+
+#include <vector>
+#include <array>
+#include <algorithm>
+
+const double EPS = 0.0000001;
+
 
 using namespace std;
 
-const double eps = 0.0000001;
+//  Template alias - C++11
+//template <size_t LEN>
+
+//  Upgraded typedef - C++11
+    using Matrix1D = std::vector<double>;
+
+//template <class T, size_t ROW, size_t COL>
+    using Matrix2D = std::vector<std::vector<double> >;
+
+
+
 
 class LinearSystem
 {
-private:
-	int Dimension;
-	double** A;
-	double* F;
-	double* X;
-public:
-	double time_simp;
-	double time_omp;
-	LinearSystem(int N)
-	{
-		srand(time(0));
-		Dimension = N;
-		A = new double*[N];
-		F = new double[N];
-		X = new double[N];
-		time_omp = 0;
+    private:
+	    unsigned int dimension;
+        Matrix2D A;
+        Matrix1D F;
+        Matrix1D X;
+
+    public:
+	    double time_simp;
+	    double time_omp;
+
+	LinearSystem(unsigned int N) {
+		srand((unsigned int) time(0));
+		dimension = N;
+
+        A.resize(N);
+        F.resize(N);
+        X.resize(N);
+
+        time_omp = 0;
 		time_simp = 0;
-		this->LoadData();
+
+        this->load_data();
 	}
-	~LinearSystem(void)
-	{
-		delete A;
-		delete F;
-		delete X;
-	}
-	void LoadData(void);
-	void CasualSolve(void);
-	void OpenMPSolve(void);
-	void ClearX(void);
+
+//	~LinearSystem(void)
+
+	void load_data();
+	void solve_casual();
+	void solve_omp();
+	void init_data();
+	void display_1d(Matrix1D&);
+	void display_2d(Matrix2D&);
 };
 
-void LinearSystem::LoadData(void)
-{
 
-	for (int i = 0; i < Dimension; i++) {
-		A[i] = new double[Dimension];
-		for (int j = 0; j < Dimension; j++) {
+void LinearSystem::load_data()
+{
+	for (size_t i = 0; i < dimension; i++) {
+        Matrix1D row(dimension);
+		A.push_back(row);
+		for (size_t j = 0; j < dimension; j++) {
 			if (i == j)
-				A[i][j] = 100 * Dimension + rand() % 300 * Dimension;
+				A[i][j] = 100 * dimension + rand() % 300 * dimension;
 			else
 				A[i][j] = 1 + rand() % 100;
 		}
@@ -61,39 +80,43 @@ void LinearSystem::LoadData(void)
 	}
 }
 
-void LinearSystem::DisplayData(double* arg) {
-	for (int i = 0; i < Dimension; i++)
+
+void LinearSystem::display_1d(Matrix1D& arg)
+{
+	for (size_t i = 0; i < dimension; i++)
 		cout << arg[i] << endl;
 	cout << "=========================";
 }
 
-void LinearSystem::DisplayData(double** arg) {
-	for (int i = 0; i < Dimension; i++) {
-		for (int g = 0; g < Dimension; g++)
+
+void LinearSystem::display_2d(Matrix2D& arg)
+{
+	for (size_t i = 0; i < dimension; i++) {
+		for (int g = 0; g < dimension; g++)
 			cout << arg[i][g] << " ";
 		cout << endl;
 	}
 	cout << endl;
 }
 
-/// N - ðàçìåðíîñòü ìàòðèöû; A[N][N] - ìàòðèöà êîýôôèöèåíòîâ, F[N] - ñòîëáåö ñâîáîäíûõ ÷ëåíîâ,
-/// X[N] - íà÷àëüíîå ïðèáëèæåíèå, îòâåò çàïèñûâàåòñÿ òàêæå â X[N];
-void LinearSystem::CasualSolve(void)
+
+/// N - размерность матрицы; A[N][N] - матрица коэффициентов, F[N] - столбец свободных членов,
+/// X[N] - начальное приближение, также ответ записывается в X[N];
+void LinearSystem::solve_casual()
 {
-	int N = Dimension, g;
-	double time1, time2; //Äëÿ çàìåðà âðåìåíè
-	double* TempX = new double[N];
+	unsigned int N = dimension;
+    int g;
+	double time1, time2;
 
-	double* F1 = new double[N];
-	double* X1 = new double[N];
-	double norm; // íîðìà, îïðåäåëÿåìàÿ êàê íàèáîëüøàÿ ðàçíîñòü êîìïîíåíò ñòîëáöà èêñîâ ñîñåäíèõ èòåðàöèé.
+    Matrix1D TempX(N);
 
-	//------------------------------------------------NO OpenMP begins--------------------------------------------------//
-	memcpy(F1, F, sizeof(F));
-	memcpy(X1, X, sizeof(X));
+// Норма, определяемая как наибольшая разность столбца иксов соседней итерации
+	double norm;
+
 	time1 = omp_get_wtime();
-	do {
-		for (int i = 0; i < N; i++) {
+
+    do {
+		for (size_t i = 0; i < N; i++) {
 			TempX[i] = F[i];
 			for (g = 0; g < N; g++) {
 				if (i != g)
@@ -107,71 +130,72 @@ void LinearSystem::CasualSolve(void)
 				norm = fabs(X[h] - TempX[h]);
 			X[h] = TempX[h];
 		}
-	} while (norm > eps);
+	}
+    while (norm > EPS);
+
 	time2 = omp_get_wtime();
-	//DisplayData(X);
-	//cout << "Time elapsed (simple): " << time2 - time1 << endl;
+	//display_data(X);
 	time_simp = time2 - time1;
-	//-----------------------------------------------Ending NO OpenMP-----------------------------------------------------//
-	delete F1;
-	delete TempX;
-	delete X1;
 }
 
-void LinearSystem::OpenMPSolve(void) {
-	int N = Dimension, g;
-	double t1, t2; //Äëÿ çàìåðà âðåìåíè
-	double* TempX = new double[N];
 
-	double* F1 = new double[N];
-	double* X1 = new double[N];
-	double norm; // íîðìà, îïðåäåëÿåìàÿ êàê íàèáîëüøàÿ ðàçíîñòü êîìïîíåíò ñòîëáöà èêñîâ ñîñåäíèõ èòåðàöèé.
-	//------------------------------------------------OpenMP begins-------------------------------------------------------//
-	memcpy(F1, F, sizeof(F));
-	memcpy(X1, X, sizeof(X));
+void LinearSystem::solve_omp()
+{
+	unsigned int N = dimension;
+    int g;
+	double t1, t2;
+
+    Matrix1D TempX(N);
+
+    double norm;
 	t1 = omp_get_wtime();
-	do {
-		///////////////////////////////////////////////////////////////////////////
-#pragma omp parallel for private(g) shared(TempX,F1,X1) 
-		for (int i = 0; i < N; i++) {
+
+	do
+    {
+    #pragma omp parallel for private(g) shared(TempX)
+		for (size_t i = 0; i < N; i++)
+        {
 			TempX[i] = F[i];
-			for (g = 0; g < N; g++) {
+			for (g = 0; g < N; g++)
+            {
 				if (i != g)
 					TempX[i] -= A[i][g] * X[g];
 			}
 			TempX[i] /= A[i][i];
 		}
-		//////////////////////////////////////////////////////////////////////////
 		norm = fabs(X[0] - TempX[0]);
 		for (int h = 0; h < N; h++) {
 			if (fabs(X[h] - TempX[h]) > norm)
 				norm = fabs(X[h] - TempX[h]);
 			X[h] = TempX[h];
 		}
-	} while (norm > eps);
+	}
+    while (norm > EPS);
+
 	t2 = omp_get_wtime();
-	//DisplayData(X);
-	//cout << "Time elapsed (openMP): " << t2 - t1 << endl;
+	//display_data(X);
 	time_omp = t2 - t1;
-	//-----------------------------------------------Ending OpenMP-----------------------------------------------------//
 }
 
-void LinearSystem::ClearX(void) {
-	for (int i = 0; i < Dimension; i++)
+
+void LinearSystem::init_data() {
+	for (size_t i = 0; i < dimension; i++)
 		X[i] = 1;
 }
+
+
 
 int main(int argc, char* argv[])
 {
 	int num; // = atoi(argv[2]);
 	cin >> num;
-	int dimension; //= atoi(argv[1]);
+	unsigned int dimension; //= atoi(argv[1]);
 	cin >> dimension;
 	omp_set_num_threads(num);
 	LinearSystem* ls = new LinearSystem(dimension);
-	ls->OpenMPSolve();
-	ls->ClearX();
-	ls->CasualSolve();
+    ls->solve_omp();
+    ls->init_data();
+    ls->solve_casual();
 	cout << "OpenMP time: " << ls->time_omp << endl << "Casual time: " << ls->time_simp;
 	return 0;
 }
